0632474e9a5c1b1d88cad928eb44b472a0450876
GiorgioAlbertoLucia <giogioalberto@gmail.com>
2026-05-13 16:44:31 +0200

    studies on the particle  source

--- femto/source_radius_parametrisation.py
+++ femto/source_radius_parametrisation.py
@@ -1,3 +1,4 @@
+from itertools import product
 
 import numpy as np
 from uncertainties import ufloat
@@ -7,7 +8,8 @@ from ROOT import kRed, kBlack, kBlue, kGreen, kMagenta, kCyan
 import sys
 sys.path.append('..')
 from utils.particles import ParticleMasses
-from torchic.utils.root import set_root_object
+from torchic.utils.root import set_root_object, init_legend
+from torchic.utils.colors import get_color
 
 def pt_to_mt(pt:float, particle:str):
     return ufloat(np.sqrt( pt**2 + ParticleMasses[particle]**2 ), 0)
@@ -30,6 +32,18 @@ PARAMETERISATION = {
     },
 }
 
+NCH_RUN2 = {
+    '0-10%':  ufloat(1764, 24),
+    '10-30%': ufloat(983, 19),
+    '30-50%': ufloat(415, 10),
+}
+
+NCH_RUN3 = {
+    '0-10%':  ufloat(1858, 34),
+    '10-30%': ufloat(1051, 21),
+    '30-50%': ufloat(455, 12),
+}
+
 MEASURED_POINTS_PT = {
     'Pr': {
         '0-10%': ufloat(0.9584, 0.3755),
@@ -40,6 +54,19 @@ MEASURED_POINTS_PT = {
         '0-10%': ufloat(2.899, 1.8083),
         '10-30%': ufloat(2.801, 1.032),
         '30-50%': ufloat(2.578, 0.9818),
+    },
+}
+
+MEASURED_POINTS_MT = {
+    'Pr': {
+        '0-10%':  ufloat(1.74, 0.),
+        '10-30%': ufloat(1.74, 0.),
+        '30-50%': ufloat(1.69, 0.),
+    },
+    'He': {
+        '0-10%':  ufloat(4.97, 0.),
+        '10-30%': ufloat(4.83, 0.),
+        '30-50%': ufloat(4.60, 0.),
     },
 }
 
@@ -65,49 +92,61 @@ def draw_source_radius():
     XMIN, XMAX = 0.9, 7.5
     NPOINTS = 100
     X = np.linspace(XMIN, XMAX, NPOINTS)
+    
 
-    for centrality in ['0-10%', '10-30%', '30-50%']:
+    for icentrality, centrality in enumerate(['0-10%', '10-30%', '30-50%']):
+        
+        a, b, c = PARAMETERISATION[centrality]['a'].n, PARAMETERISATION[centrality]['b'].n, PARAMETERISATION[centrality]['c'].n
+        a_err, b_err, c_err = PARAMETERISATION[centrality]['a'].s, PARAMETERISATION[centrality]['b'].s, PARAMETERISATION[centrality]['c'].s
+        combos = np.array(list(product([a-a_err, a+a_err], [b-b_err, b+b_err], [c-c_err, c+c_err])))
 
+        pr_point = TGraphAsymmErrors(1)
+        he_point = TGraphAsymmErrors(1)
+        
         funcs[centrality] = TF1(f'func_{centrality}', f'[0] + [1]*x^[2]', XMIN, XMAX)
-        funcs[centrality].SetParameters(
-            PARAMETERISATION[centrality]['a'].n,
-            PARAMETERISATION[centrality]['b'].n,
-            PARAMETERISATION[centrality]['c'].n
-        )
-        funcs[centrality].SetParErrors(
-            np.array([
-                PARAMETERISATION[centrality]['a'].s,
-                PARAMETERISATION[centrality]['b'].s,
-                PARAMETERISATION[centrality]['c'].s
-            ]))
+        funcs[centrality].SetParameters(a, b, c)
         
-        funcs_plus_error[centrality] = TF1(f'func_plus_error{centrality}', f'[0] + [1]*x^[2]', XMIN, XMAX)
-        funcs_plus_error[centrality].SetParameters(
-            PARAMETERISATION[centrality]['a'].n + PARAMETERISATION[centrality]['a'].s,
-            PARAMETERISATION[centrality]['b'].n + PARAMETERISATION[centrality]['b'].s,
-            PARAMETERISATION[centrality]['c'].n + PARAMETERISATION[centrality]['c'].s
-        )
+        x_pr = MEASURED_POINTS_MT['Pr'][centrality].n
+        y_pr = funcs[centrality].Eval(x_pr)
+        max_y_pr = np.max(combos[:,0] + combos[:,1] * x_pr**combos[:,2])
+        min_y_pr = np.min(combos[:,0] + combos[:,1] * x_pr**combos[:,2])
+    
+        pr_point.SetPoint(0, x_pr, y_pr)
+        pr_point.SetPointError(0, 0, 0, y_pr - min_y_pr, max_y_pr - y_pr)
+        points['Pr'][centrality] = pr_point
         
-        for particle in ['Pr', 'He']:
-            points[particle][centrality] = TGraphErrors(1)
-            points[particle][centrality].SetPoint(0, MEASURED_POINTS[particle][centrality].n, funcs[centrality].Eval(MEASURED_POINTS[particle][centrality].n))
-        print(f'Measured {centrality}: {MEASURED_POINTS[particle][centrality].n}, Rp: {points['Pr'][centrality].GetY()[0]}, Rhe3: {points['He'][centrality].GetY()[0]}')
-
+        x_he = MEASURED_POINTS_MT['He'][centrality].n
+        y_he = funcs[centrality].Eval(x_he)
+        max_y_he = np.max(combos[:,0] + combos[:,1] * x_he**combos[:,2])
+        min_y_he = np.min(combos[:,0] + combos[:,1] * x_he**combos[:,2])
+    
+        he_point.SetPoint(0, x_he, y_he)
+        he_point.SetPointError(0, 0, 0, y_he - min_y_he, max_y_he - y_he)
+        points['He'][centrality] = he_point
+        
         bands[centrality] = TGraphAsymmErrors(NPOINTS)
-        for ix, x in enumerate(X):
-            #dfa = 1
-            #dfb = x**PARAMETERISATION[centrality]['c'].n if x > 0 else 0
-            #dfc = np.log(x) * PARAMETERISATION[centrality]['b'].n * x**(PARAMETERISATION[centrality]['c'].n - 1) if x > 0 else 0
-            #err = np.sqrt((dfa * PARAMETERISATION[centrality]['a'].s)**2 + 
-            #              (dfb * PARAMETERISATION[centrality]['b'].s)**2 + 
-            #              (dfc * PARAMETERISATION[centrality]['c'].s)**2)
-
-            err = funcs_plus_error[centrality].Eval(x) - funcs[centrality].Eval(x)
-
-            bands[centrality].SetPoint(ix, x, funcs[centrality].Eval(x))
-            bands[centrality].SetPointError(ix, 0., 0., err, err)
-
-
+        for i, x in enumerate(X):
+            y = funcs[centrality].Eval(x)
+            max_y = np.max(combos[:,0] + combos[:,1] * x**combos[:,2])
+            min_y = np.min(combos[:,0] + combos[:,1] * x**combos[:,2])
+            bands[centrality].SetPoint(i, x, y)
+            bands[centrality].SetPointError(i, 0, 0, y - min_y, max_y - y)
+        
+        
+    Nch_pr_graph = TGraphAsymmErrors(3)
+    Nch_he_graph = TGraphAsymmErrors(3)
+    for icentrality, centrality in enumerate(['0-10%', '10-30%', '30-50%']):
+        
+        nch = NCH_RUN2[centrality]
+        
+        Nch_pr_graph.SetPoint(icentrality, nch.n**(1/3), points['Pr'][centrality].GetY()[0])
+        Nch_pr_graph.SetPointError(icentrality, nch.s * (1/3) * nch.n**(-2/3), nch.s * (1/3) * nch.n**(-2/3), 
+                                   points['Pr'][centrality].GetErrorYlow(0), points['Pr'][centrality].GetErrorYhigh(0))
+        
+        Nch_he_graph.SetPoint(icentrality, nch.n**(1/3), points['He'][centrality].GetY()[0])
+        Nch_he_graph.SetPointError(icentrality, nch.s * (1/3) * nch.n**(-2/3), nch.s * (1/3) * nch.n**(-2/3), 
+                                   points['He'][centrality].GetErrorYlow(0), points['He'][centrality].GetErrorYhigh(0))
+        
 
     canvas = TCanvas('canvas', 'Source Radius Parameterisation', 800, 600)
     gStyle.SetOptStat(0)
@@ -120,41 +159,116 @@ def draw_source_radius():
     watermark.AddText('#bf{ALICE Run 3}')
     watermark.AddText('#bf{Pb-Pb  #it{#sqrt{s_{NN}}} = 5.36 TeV}')
     
-    legend = TLegend(0.5, 0.68, 0.88, 0.88)
-    legend.SetBorderSize(0)
-    legend.SetFillStyle(0)
-    legend.SetNColumns(3)
-    legend.SetTextSize(0.03)
-
+    legend = init_legend(0.5, 0.68, 0.88, 0.88, text_size=0.03, n_columns=3)
+    
     results_panel = TPaveText(0.38, 0.48, 0.88, 0.64, 'NDC')
     results_panel.SetFillColor(0)
     results_panel.SetBorderSize(0)
     results_panel.SetTextSize(0.038)
     results_panel.AddText('       #bf{#bf{#it{R}_{p} (fm)}  #bf{#it{R}_{^{3}He} (fm)}}')
 
-    for color, centrality in zip([kRed, kBlue, kGreen], ['0-10%', '10-30%', '30-50%']):
+    for icentrality, centrality in enumerate(['0-10%', '10-30%', '30-50%']):
 
-        bands[centrality].SetFillColor(color)
-        bands[centrality].SetFillStyle(3001)
-        bands[centrality].Draw('3 same')
+        #bands[centrality].SetFillColor(color)
+        #bands[centrality].SetFillStyle(3001)
+        #bands[centrality].Draw('3 same')
+        
+        set_root_object(funcs[centrality], line_color=get_color(icentrality), line_width=2)
+        funcs[centrality].Draw('same')
 
-        set_root_object(points['Pr'][centrality], marker_color=color, marker_size=2, marker_style=20)
+        set_root_object(points['Pr'][centrality], marker_color=get_color(icentrality), marker_size=1.5, marker_style=20)
         points['Pr'][centrality].Draw('p same')
-        set_root_object(points['He'][centrality], marker_color=color, marker_size=3, marker_style=33)
+        set_root_object(points['He'][centrality], marker_color=get_color(icentrality), marker_size=2, marker_style=33)
         points['He'][centrality].Draw('p same')
         
-        legend.AddEntry(bands[centrality], centrality, 'f')
+        set_root_object(bands[centrality], fill_color=get_color(icentrality), line_color=0, fill_style=3001, fill_color_alpha=(get_color(icentrality), 0.3))
+        bands[centrality].Draw('3 same')
+        
+        #legend.AddEntry(bands[centrality], centrality, 'f')
         legend.AddEntry(points['Pr'][centrality], f'#it{{R}}_{{p}}', 'p')
         legend.AddEntry(points['He'][centrality], f'#it{{R}}_{{^{{3}}He}}', 'p')
 
         results_panel.AddText(f'#bf{{{centrality}:}}   #bf{{{points['Pr'][centrality].GetY()[0]:.2f}}}    #bf{{{points['He'][centrality].GetY()[0]:.2f}}}')
-
         print(f'Centrality {centrality}: Rp = {points["Pr"][centrality].GetY()[0]:.2f} fm, Rhe3 = {points["He"][centrality].GetY()[0]:.2f} fm, Rsource = {np.sqrt((points["Pr"][centrality].GetY()[0])**2 + (points["He"][centrality].GetY()[0])**2):.2f} fm')
 
     watermark.Draw()
     results_panel.Draw()
     legend.Draw('same')
-    canvas.SaveAs('figures/source_radius_parameterisation.pdf')
+    canvas.SaveAs('figures/source_radius_parameterisation_new.pdf')
+    
+    canvas_nch = TCanvas('canvas_nch', 'Source Radius Parameterisation Nch', 800, 600)
+    gStyle.SetOptStat(0)
+    hframe_nch = canvas.DrawFrame(5, 1.2, 15, 6.5, ';#LTd#it{N}_{ch}/d#it{#eta} #GT^{1/3}_{|#it{#eta}|<0.5} (GeV/#it{c}^{2});#it{R} (fm)')
+    legend_nch = init_legend(0.54, 0.18, 0.88, 0.34, text_size=0.03, n_columns=2)
+    NCH_CUBEROOT_RANGE = np.linspace(5, 15, 100)
+    
+    funcs_nch = {}
+    run3_points = {'Pr': TGraphAsymmErrors(3), 'He': TGraphAsymmErrors(3) }
+    bands = {'Pr': TGraphAsymmErrors(NCH_CUBEROOT_RANGE.size), 'He': TGraphAsymmErrors(NCH_CUBEROOT_RANGE.size)}
+    label = {'Pr': '#it{R}_{p}', 'He': '#it{R}_{^{3}He}'}
+    
+    for iparticle, particle in enumerate(['Pr', 'He']):
+        
+        graph = Nch_pr_graph if particle == 'Pr' else Nch_he_graph
+        funcs_nch[particle] = TF1(f'func_nch_{particle}', f'[0] + [1]*x', 5, 15)
+        fit_result = graph.Fit(funcs_nch[particle], 'RMS+')
+        covariance_matrix = fit_result.GetCovarianceMatrix()
+        covariance = covariance_matrix(0,1)
+        
+        print('Parameterisation for particle', particle)
+        covariance_matrix.Print()
+        
+        for icentrality, centrality in enumerate(['0-10%', '10-30%', '30-50%']):
+            nch = NCH_RUN3[centrality]
+            radius = funcs_nch[particle].Eval(nch.n**(1/3))
+            
+            #a, b = funcs_nch[particle].GetParameter(0), funcs_nch[particle].GetParameter(1) 
+            #a_err, b_err = funcs_nch[particle].GetParError(0), funcs_nch[particle].GetParError(1)
+            #combos = np.array(list(product([a-a_err, a+a_err], [b-b_err, b+b_err])))
+            #max_y = np.max(combos[:,0] + combos[:,1] * nch.n**(1/3))
+            #min_y = np.min(combos[:,0] + combos[:,1] * nch.n**(1/3))
+            #
+            #run3_points[particle].SetPoint(icentrality, nch.n**(1/3), radius)
+            #run3_points[particle].SetPointError(icentrality, nch.s * (1/3) * nch.n**(-2/3), nch.s * (1/3) * nch.n**(-2/3), radius - min_y, max_y - radius)
+            #print(f'Centrality {centrality}: Nch = {nch.n:.0f}, Radius = {radius:.2f} + {max_y - radius:.2f} / - {radius - min_y:.2f} fm')
+            
+            radius_err = np.sqrt( (funcs_nch[particle].GetParError(0))**2 + (nch.n**(1/3) * funcs_nch[particle].GetParError(1))**2 - 2 * nch.n**(1/3) * covariance )
+            run3_points[particle].SetPoint(icentrality, nch.n**(1/3), radius)
+            run3_points[particle].SetPointError(icentrality, nch.s * (1/3) * nch.n**(-2/3), nch.s * (1/3) * nch.n**(-2/3), radius_err, radius_err)
+            print(f'Centrality {centrality}: Nch = {nch.n:.0f}, Radius = {radius:.2f} ± {radius_err:.2f} fm')
+            
+        for i, nch_cuberoot in enumerate(NCH_CUBEROOT_RANGE):
+            y = funcs_nch[particle].Eval(nch_cuberoot)
+            
+            #a, b = funcs_nch[particle].GetParameter(0), funcs_nch[particle].GetParameter(1) 
+            #a_err, b_err = funcs_nch[particle].GetParError(0), funcs_nch[particle].GetParError(1)
+            #combos = np.array(list(product([a-a_err, a+a_err], [b-b_err, b+b_err])))
+            #max_y = np.max(combos[:,0] + combos[:,1] * nch)
+            #min_y = np.min(combos[:,0] + combos[:,1] * nch)
+            #
+            #bands[particle].SetPoint(i, nch, y)
+            #bands[particle].SetPointError(i, 0, 0, y - min_y, max_y - y)
+            
+            y_err = np.sqrt( (funcs_nch[particle].GetParError(0))**2 + (nch_cuberoot * funcs_nch[particle].GetParError(1))**2 - 2 * nch_cuberoot * covariance )
+            bands[particle].SetPoint(i, nch_cuberoot, y)
+            bands[particle].SetPointError(i, 0, 0, y_err, y_err)
+        
+        set_root_object(graph, marker_color=get_color(iparticle), marker_size=(1.5 if particle == 'Pr' else 2), 
+                        marker_style=20 if particle == 'Pr' else 33, line_color=get_color(iparticle))
+        set_root_object(funcs_nch[particle], line_color=get_color(iparticle), line_width=2)
+        set_root_object(run3_points[particle], marker_color=get_color(iparticle), marker_size=(1.5 if particle == 'Pr' else 2), 
+                        marker_style=24 if particle == 'Pr' else 27, line_color=get_color(iparticle))
+        set_root_object(bands[particle], fill_color=get_color(iparticle), line_color=0, fill_style=3001, fill_color_alpha=(get_color(iparticle), 0.3))
+        
+        graph.Draw('p same')
+        funcs_nch[particle].Draw('same')
+        run3_points[particle].Draw('p same')
+        bands[particle].Draw('3 same')
+        legend_nch.AddEntry(graph, f'Run 2: {label[particle]}', 'p')
+        legend_nch.AddEntry(run3_points[particle], f'Run 3: {label[particle]}', 'p')
+    
+    legend_nch.Draw('same')
+    canvas_nch.SaveAs('figures/source_radius_parameterisation_nch_new.pdf')
 
 if __name__ == '__main__':
 
---
+++ phase_space_studies/mt_distributions.py
@@ -0,0 +1,164 @@
+import sys
+import yaml
+import argparse
+
+import ROOT
+from ROOT import TFile, TChain, gInterpreter, RDataFrame
+
+from torchic.utils.terminal_colors import TerminalColors as tc
+
+sys.path.append('..')
+from utils.particles import ParticleMasses
+
+from include.load_parameters import load_parametrisation
+
+gInterpreter.ProcessLine(f'#include "../include/Common.h"')
+
+ROOT.EnableImplicitMT(30)
+ROOT.gROOT.SetBatch(True)
+
+
+def prepare_selections(selections):
+
+    base_selection = ''
+    if config.get('like_sign', True):  
+        base_selection = '(fSignedPtHe3 > 0 && fSignedPtHad > 0) || (fSignedPtHe3 < 0 && fSignedPtHad < 0)'
+    else:
+        base_selection = '(fSignedPtHe3 > 0 && fSignedPtHad < 0) || (fSignedPtHe3 < 0 && fSignedPtHad > 0)'
+    
+    selection = selections[0]
+    for sel in selections[1:]:
+        selection += (' && ' + sel)
+
+    return base_selection, selection
+
+def prepare_input_tchain(config:dict):
+
+    input_data = config['input_data']
+    tree_names = config['tree_names']
+    mode = config['mode']
+
+    file_data_list = input_data if isinstance(input_data, list) else [input_data]
+    tree_name = tree_names if isinstance(tree_names, str) else tree_names[0]
+    chain_data = TChain('tchain')
+
+    additional_chains = []
+    if isinstance(tree_names, list) and len(tree_names) > 1:
+        for idx, tname in enumerate(tree_names[1:]):
+            additional_chain = TChain(f'tchain_friend_{idx}')
+            additional_chains.append(additional_chain)
+
+    for file_name in file_data_list:
+        fileData = TFile(file_name)
+
+        if mode == 'DF':
+            for key in fileData.GetListOfKeys():
+                key_name = key.GetName()
+                if 'DF_' in key_name :
+                    chain_data.Add(f'{file_name}/{key_name}/{tree_name}')
+                    for idx, additional_chain in enumerate(additional_chains):
+                        additional_chain.Add(f'{file_name}/{key_name}/{tree_names[idx+1]}')
+            
+        elif mode == 'tree':
+            print(f'Adding {tc.CYAN+tc.UNDERLINE}{file_name}/{tree_name}{tc.RESET} to the chain')
+            chain_data.Add(f'{file_name}/{tree_name}')
+            for idx, additional_chain in enumerate(additional_chains):
+                additional_chain.Add(f'{file_name}/{tree_names[idx+1]}')
+
+    for idx, additional_chain in enumerate(additional_chains):
+        chain_data.AddFriend(additional_chain)
+
+    return chain_data, additional_chains
+
+def prepare_rdataframe(chain_data: TChain, base_selection: str, selection: str):
+   
+    rdf = RDataFrame(chain_data)
+    print(tc.GREEN+'\nDataset columns'+tc.RESET)
+    print(tc.UNDERLINE+tc.CYAN+f'{rdf.GetColumnNames()}'+tc.RESET)
+    
+    # TPC
+    if 'fNSigmaTPCHadPr' in rdf.GetColumnNames():
+        rdf = rdf.Define('fNSigmaTPCHad', 'fNSigmaTPCHadPr')
+    elif 'fNSigmaTPCHad' not in rdf.GetColumnNames():
+        raise RuntimeError("Neither fNSigmaTPCHadPr nor fNSigmaTPCHad found in dataset!")
+    
+    # TOF
+    if 'fNSigmaTOFHadPr' in rdf.GetColumnNames() and 'fNSigmaTOFHad' in rdf.GetColumnNames():
+        rdf = rdf.Define('fNSigmaTOFHad', 'fNSigmaTOFHadPr')
+    elif 'fNSigmaTOFHad' not in rdf.GetColumnNames():
+        #if 'fNSigmaTOFHadPr' not in rdf.GetColumnNames():
+        #    rdf = rdf.Define('fNSigmaTOFHad', 'ComputeNsigmaTOFPr(std::abs(fPtHad), fMassTOFHad)')
+        #else:
+        #    rdf = rdf.Define('fNSigmaTOFHad', 'fNSigmaTOFHadPr')
+       rdf = rdf.Define('fNSigmaTOFHad', 'ComputeNsigmaTOFPr(std::abs(fPtHad), fMassTOFHad)')
+    
+      # Recalibration
+      # Correct for PID in tracking
+      
+    rdf = rdf.Define('fSignedPtHad', 'fPtHad') \
+      .Define('fSignHe3', 'fPtHe3/std::abs(fPtHe3)') \
+      .Redefine('fPtHe3', 'std::abs(fPtHe3)') \
+      .Redefine('fPtHad', 'std::abs(fPtHad)') \
+      .Redefine('fPtHe3', '(fPIDtrkHe3 == 7) || (fPIDtrkHe3 == 8) || (fPtHe3 > 2.5) ? fPtHe3 : CorrectPidTrkHe(fPtHe3)') \
+      .Redefine('fInnerParamTPCHe3', 'fInnerParamTPCHe3 * 2') \
+      .Redefine('fInnerParamTPCHe3', '(fPIDtrkHe3 == 7) || (fPIDtrkHe3 == 8) || (fPtHe3 > 2.5) ? fInnerParamTPCHe3 : CorrectPidTrkHe(fInnerParamTPCHe3, false)') \
+      .Define('fSignedPtHe3', 'fPtHe3 * fSignHe3') \
+      .Define(f'fEHe3', f'std::sqrt((fPtHe3 * std::cosh(fEtaHe3))*(fPtHe3 * std::cosh(fEtaHe3)) + {ParticleMasses["He"]}*{ParticleMasses["He"]})') \
+      .Define(f'fEHad', f'std::sqrt((fPtHad * std::cosh(fEtaHad))*(fPtHad * std::cosh(fEtaHad)) + {ParticleMasses["Pr"]}*{ParticleMasses["Pr"]})') \
+      .Define('fDeltaEta', 'fEtaHe3 - fEtaHad') \
+      .Define('fDeltaPhi', 'fPhiHe3 - fPhiHad') \
+      .Define('fClusterSizeCosLamHe3', 'ComputeAverageClusterSize(fItsClusterSizeHe3) / cosh(fEtaHe3)') \
+      .Define('fClusterSizeCosLamHad', 'ComputeAverageClusterSize(fItsClusterSizeHad) / cosh(fEtaHad)') \
+      .Define('fExpectedClusterSizeHe3', 'ComputeExpectedClusterSizeCosLambdaHe(fPtHe3 * std::cosh(fEtaHe3))') \
+      .Define('fExpectedClusterSizeHad', 'ComputeExpectedClusterSizeCosLambdaPr(fPtHad * std::cosh(fEtaHad))') \
+      .Define('fNSigmaITSHe3', 'ComputeNsigmaITSHe(fPtHe3 * std::cosh(fEtaHe3), fClusterSizeCosLamHe3)') \
+      .Define('fNSigmaITSHad', 'ComputeNsigmaITSPr(fPtHad * std::cosh(fEtaHad), fClusterSizeCosLamHad)') \
+      .Redefine('fNSigmaTPCHe3', 'ComputeNsigmaTPCHe(std::abs(fInnerParamTPCHe3), fSignalTPCHe3, false, true)') \
+      .Define('fNSigmaTPCPi', 'ComputeNsigmaTPCPi(std::abs(fInnerParamTPCHad), fSignalTPCHad)') \
+      .Define('fNSigmaDCAxyHe3', 'ComputeNsigmaDCAxyHe(fPtHe3, fDCAxyHe3)') \
+      .Define('fNSigmaDCAzHe3', 'ComputeNsigmaDCAzHe(fPtHe3, fDCAzHe3)') \
+      .Define('fNSigmaDCAxyHad', 'ComputeNsigmaDCAxyPr(fPtHad, fDCAxyHad)') \
+      .Define('fNSigmaDCAzHad', 'ComputeNsigmaDCAzPr(fPtHad, fDCAzHad)') \
+      .Filter(base_selection).Filter(selection) \
+      .Define('fMtHad', f'std::sqrt((fPtHad)*(fPtHad) + {ParticleMasses["Pr"]}*{ParticleMasses["Pr"]})') \
+      .Define('fMtHe3', f'std::sqrt((fPtHe3)*(fPtHe3) + {ParticleMasses["He"]}*{ParticleMasses["He"]})')
+    
+    return rdf
+
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Prepare data for femtoscopic analysis')
+    parser.add_argument('--config', type=str, default='config/config_prepare.yml', help='Path to the configuration YAML file')  
+    parser.add_argument('--mode', type=str, default='same_event', choices=['same_event', 'mixed_event'], help='Mode of analysis: same_event or mixed_event')
+    args = parser.parse_args()
+
+    config_file = args.config
+    config = yaml.safe_load(open(config_file, 'r'))
+    
+    load_parametrisation(config)  # Load parametrisation into Common.h
+    
+    selections = config.get('selections', [])
+    config = config[args.mode]  # Use the specific mode section from the config
+
+    base_selection, selection = prepare_selections(selections)
+    chain_data, additional_chain_data = prepare_input_tchain(config)
+    rdf = prepare_rdataframe(chain_data, base_selection, selection)
+
+    output_file = ROOT.TFile('output/mt.root', "RECREATE")
+    
+    for centrality, centrality_selection in {'010': 'fCentralityFT0C >= 0 && fCentralityFT0C < 10', 
+                                             '1030': 'fCentralityFT0C >= 10 && fCentralityFT0C < 30', 
+                                             '3050': 'fCentralityFT0C >= 30 && fCentralityFT0C < 50'}.items():
+        output_dir = output_file.mkdir(centrality)
+        hist_centrality_had = rdf.Filter(centrality_selection).Histo1D(('hMtHad', ';m_{T} (GeV/c^{2}); Counts', 200, 0, 10), 'fMtHad').GetValue()
+        hist_centrality_he3 = rdf.Filter(centrality_selection).Histo1D(('hMtHe3', ';m_{T} (GeV/c^{2}); Counts', 200, 0, 10), 'fMtHe3').GetValue()
+        
+        output_dir.cd()
+        hist_centrality_had.Write()
+        hist_centrality_he3.Write()
+        
+    output_file.Close()
+
+  
