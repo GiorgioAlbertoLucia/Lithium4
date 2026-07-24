ac2b75317e6ebc5eed3903732d55af71ccf9e94b
GiorgioAlbertoLucia <giogioalberto@gmail.com>
2026-07-24 15:57:20 +0200

    lambda parameter task is now loading all data properly

--- calibration/compute_lambda_parameters.py
+++ calibration/compute_lambda_parameters.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 import yaml
 import ROOT
 from ROOT import TFile, TChain, gInterpreter, RDataFrame
@@ -18,19 +19,93 @@ ROOT.EnableImplicitMT(10)
 ROOT.gROOT.SetBatch(True)
 
 
-def prepare_selections(selections):
+PARAMETRISATIONS = {
+    '2023': {
+        'kDCAxyResolutionParams_Pr': [8.19e-5, 0.004, 0.0026, 1.1741],
+        'kDCAxyResolutionParams_He': [1.09e-4, 0.0011, 0.0065, 1.0399],
+        'kDCAzResolutionParams_Pr': [1.18e-4, 0.0020, 0.0025, 1.3460],
+        'kDCAzResolutionParams_He': [9.36e-5, 0.0019, 0.0080, 1.416],
 
-    base_selection = ''
-    if config.get('like_sign', True):  
-        base_selection = '(fSignedPtHe3 > 0 && fSignedPtHad > 0) || (fSignedPtHe3 < 0 && fSignedPtHad < 0)'
-    else:
-        base_selection = '(fSignedPtHe3 > 0 && fSignedPtHad < 0) || (fSignedPtHe3 < 0 && fSignedPtHad > 0)'
-    
-    selection = selections[0]
-    for sel in selections[1:]:
-        selection += (' && ' + sel)
+        # PbPb 23 pass5
+        'kHeTPCParams': [-244.627, 0.100002, 1.14056, 0.365353, 3.26339],
+        'kHeTPCResolution': 0.061,
+        'kHeTPCResidualType': 'double_gaus',
+        'kHeTPCParamsResiduals': [87.1178, 0.438363, 0.0814248, -16.9173, 0.754105, 0.285692],
 
-    return base_selection, selection
+        'kITSParams_Pr': [1.0228, 1.9634, 2.2081], # Pr from 'Ka' fitting
+        'kITSParams_He': [2.5734, 1.3620, 5.1383],  # He 
+        'kITSResolutionParams_Pr': [0.1575, 0., 0.],
+        'kITSResolutionParams_He': [0.1124, -0.0100, 0.],
+
+        'kHePidTrkParamsPt': [0.3101, -0.1759, 0.0262],
+        'kHePidTrkParamsP': [0., 0., 0.],
+    }, 
+    '2024': {
+        'kDCAxyResolutionParams_Pr': [8.19e-5, 0.004, 0.0026, 1.1741],
+        'kDCAxyResolutionParams_He': [1.09e-4, 0.0011, 0.0065, 1.0399],
+        'kDCAzResolutionParams_Pr': [1.18e-4, 0.0020, 0.0025, 1.3460],
+        'kDCAzResolutionParams_He': [9.36e-5, 0.0019, 0.0080, 1.416],
+
+        # 24_pass3
+        'kHeTPCParams': [-190.723, 0.100000, 1.45622, 0.944291, 2.96807],
+        'kHeTPCResolution': 0.059,
+        'kHeTPCResidualType': 'exp',
+        'kHeTPCParamsResiduals': [-19.1089, 5.27807, 0.698301, 0, 0, 0],
+
+        'kITSParams_Pr': [1.0228, 1.9634, 2.2081], # Pr from 'Ka' fitting
+        'kITSParams_He': [2.5734, 1.3620, 5.1383],  # He 
+        'kITSResolutionParams_Pr': [0.1575, 0., 0.],
+        'kITSResolutionParams_He': [0.1124, -0.0100, 0.],
+
+        'kHePidTrkParamsPt': [0.3101, -0.1759, 0.0262],
+        'kHePidTrkParamsP': [0., 0., 0.],
+    }, 
+    '2025': {
+        'kDCAxyResolutionParams_Pr': [8.19e-5, 0.004, 0.0026, 1.1741],
+        'kDCAxyResolutionParams_He': [1.09e-4, 0.0011, 0.0065, 1.0399],
+        'kDCAzResolutionParams_Pr': [1.18e-4, 0.0020, 0.0025, 1.3460],
+        'kDCAzResolutionParams_He': [9.36e-5, 0.0019, 0.0080, 1.416],
+
+        # 25_pass1
+        'kHeTPCParams': [-208.928, 0.100000, 1.29761, 0.756458, 3.04256],
+        'kHeTPCResolution': 0.059,
+        'kHeTPCResidualType': 'double_gaus',
+        'kHeTPCParamsResiduals': [200.000, 0.362588, 0.0235587, -32.5300, 0.400000, 0.247265],
+
+        'kITSParams_Pr': [1.0228, 1.9634, 2.2081], # Pr from 'Ka' fitting
+        'kITSParams_He': [2.5734, 1.3620, 5.1383],  # He 
+        'kITSResolutionParams_Pr': [0.1575, 0., 0.],
+        'kITSResolutionParams_He': [0.1124, -0.0100, 0.],
+
+        'kHePidTrkParamsPt': [0.3101, -0.1759, 0.0262],
+        'kHePidTrkParamsP': [0., 0., 0.],
+    }, 
+        
+    }
+
+selections = [
+    '((fChi2TPCHad < 4))',
+    '(std::abs(fEtaHe3) < 0.9)',
+    '(std::abs(fEtaHad) < 0.9)',
+    '(fNClsTPCHe3 > 110)',
+
+    '((fPtHe3 < 2.5) || (fPIDtrkHe3 == 7))',
+    '((-2. < fNSigmaTPCHe3) && (fNSigmaTPCHe3 < 3.))',
+    '(std::abs(fNSigmaDCAxyHe3) < 5)',
+    '(std::abs(fNSigmaDCAzHe3) < 5)',
+    '(fNSigmaITSHe3 > -2.)',
+
+    '(std::abs(fNSigmaTPCHad) < 2)',
+    '((std::abs(fPtHad) < 0.8) || (std::abs(fNSigmaTOFHad) < 2))',  # TOF hard cut
+    '(fNSigmaITSHad > -2.)',
+    '(std::abs(fNSigmaDCAxyHad) < 5)',
+    '(std::abs(fNSigmaDCAzHad) < 5)',
+]
+#selections = conf['selections']
+base_selection = selections[0]
+for sel in selections[1:]:
+    base_selection += (' && ' + sel)
+print(f'Selection: {base_selection}')
 
 def prepare_input_tchain(config:dict):
 
@@ -95,68 +170,78 @@ def prepare_rdataframe(chain_data: TChain, base_selection: str, selection: str):
       # Recalibration
       # Correct for PID in tracking
       
-    rdf = rdf.Define('fSignedPtHad', 'fPtHad') \
-      .Define('fSignHe3', 'fPtHe3/std::abs(fPtHe3)') \
-      .Redefine('fPtHe3', 'std::abs(fPtHe3)') \
-      .Redefine('fPtHad', 'std::abs(fPtHad)') \
-      .Redefine('fPtHe3', '(fPIDtrkHe3 == 7) || (fPIDtrkHe3 == 8) || (fPtHe3 > 2.5) ? fPtHe3 : CorrectPidTrkHe(fPtHe3)') \
-      .Redefine('fInnerParamTPCHe3', 'fInnerParamTPCHe3 * 2') \
-      .Redefine('fInnerParamTPCHe3', '(fPIDtrkHe3 == 7) || (fPIDtrkHe3 == 8) || (fPtHe3 > 2.5) ? fInnerParamTPCHe3 : CorrectPidTrkHe(fInnerParamTPCHe3, false)') \
-      .Define('fSignedPtHe3', 'fPtHe3 * fSignHe3') \
-      .Define(f'fEHe3', f'std::sqrt((fPtHe3 * std::cosh(fEtaHe3))*(fPtHe3 * std::cosh(fEtaHe3)) + {ParticleMasses["He"]}*{ParticleMasses["He"]})') \
-      .Define(f'fEHad', f'std::sqrt((fPtHad * std::cosh(fEtaHad))*(fPtHad * std::cosh(fEtaHad)) + {ParticleMasses["Pr"]}*{ParticleMasses["Pr"]})') \
-      .Define('fDeltaEta', 'fEtaHe3 - fEtaHad') \
-      .Define('fDeltaPhi', 'fPhiHe3 - fPhiHad') \
-      .Define('fClusterSizeCosLamHe3', 'ComputeAverageClusterSize(fItsClusterSizeHe3) / cosh(fEtaHe3)') \
-      .Define('fClusterSizeCosLamHad', 'ComputeAverageClusterSize(fItsClusterSizeHad) / cosh(fEtaHad)') \
-      .Define('fExpectedClusterSizeHe3', 'ComputeExpectedClusterSizeCosLambdaHe(fPtHe3 * std::cosh(fEtaHe3))') \
-      .Define('fExpectedClusterSizeHad', 'ComputeExpectedClusterSizeCosLambdaPr(fPtHad * std::cosh(fEtaHad))') \
-      .Define('fNSigmaITSHe3', 'ComputeNsigmaITSHe(fPtHe3 * std::cosh(fEtaHe3), fClusterSizeCosLamHe3)') \
-      .Define('fNSigmaITSHad', 'ComputeNsigmaITSPr(fPtHad * std::cosh(fEtaHad), fClusterSizeCosLamHad)') \
-      .Redefine('fNSigmaTPCHe3', 'ComputeNsigmaTPCHe(std::abs(fInnerParamTPCHe3), fSignalTPCHe3, false, true)') \
-      .Define('fNSigmaTPCPi', 'ComputeNsigmaTPCPi(std::abs(fInnerParamTPCHad), fSignalTPCHad)') \
-      .Define('fNSigmaDCAxyHe3', 'ComputeNsigmaDCAxyHe(fPtHe3, fDCAxyHe3)') \
-      .Define('fNSigmaDCAzHe3', 'ComputeNsigmaDCAzHe(fPtHe3, fDCAzHe3)') \
-      .Define('fNSigmaDCAxyHad', 'ComputeNsigmaDCAxyPr(fPtHad, fDCAxyHad)') \
-      .Define('fNSigmaDCAzHad', 'ComputeNsigmaDCAzPr(fPtHad, fDCAzHad)') \
-      .Filter(base_selection).Filter(selection) \
-      .Define('fKstar', f'ComputeKstar(fPtHe3, fEtaHe3, fPhiHe3, {ParticleMasses["He"]}, fPtHad, fEtaHad, fPhiHad, {ParticleMasses["Pr"]})') \
-      .Define('fPxLi', 'fPtHe3 * std::cos(fPhiHe3) + fPtHad * std::cos(fPhiHad)') \
-      .Define('fPyLi', 'fPtHe3 * std::sin(fPhiHe3) + fPtHad * std::sin(fPhiHad)') \
-      .Define('fPzLi', 'fPtHe3 * std::sinh(fEtaHe3) + fPtHad * std::sinh(fEtaHad)') \
-      .Define('fELi', 'fEHe3 + fEHad') \
-      .Define('fPLi', 'sqrt(fPxLi*fPxLi + fPyLi*fPyLi + fPzLi*fPzLi)') \
-      .Define('fPtLi', 'sqrt(fPxLi*fPxLi + fPyLi*fPyLi)') \
-      .Define('fKt', 'fPtLi / 2') \
-      .Define('fMt', f'std::sqrt((fPtLi/4)*(fPtLi/4) + {ParticleMasses["Li"]}*{ParticleMasses["Li"]})') \
-      .Define('fEtaLi', 'std::acosh(fPLi / fELi)') \
-      .Define('fPhiLi', 'std::atan2(fPyLi, fPxLi)') \
-      .Define('fSignedPtLi', 'fPtLi * fSignHe3') \
-      .Define('fMassInvLi', 'std::sqrt(fELi*fELi - fPLi*fPLi)') \
-      .Define('fMassTLi', 'std::sqrt(fELi*fELi - fPtLi*fPtLi)') \
-    
+      # close pair rejection
+      #.Define('fDeltaEta', 'fEtaHe3 - fEtaHad')
+      #.Define('fDeltaPhi', 'fPhiHe3 - fPhiHad')
+      
+    rdf = (rdf.Define('fSignedPtHad', 'fPtHad')
+      .Define('fSignHe3', 'fPtHe3/std::abs(fPtHe3)')
+      .Redefine('fPtHe3', 'std::abs(fPtHe3)')
+      .Redefine('fPtHad', 'std::abs(fPtHad)')
+      .Redefine('fPtHe3', '(fPIDtrkHe3 == 7) || (fPIDtrkHe3 == 8) || (fPtHe3 > 2.5) ? fPtHe3 : CorrectPidTrkHe(fPtHe3)')
+      .Redefine('fInnerParamTPCHe3', 'fInnerParamTPCHe3 * 2')
+      .Define('fSignedPtHe3', 'fPtHe3 * fSignHe3')
+      .Define(f'fEHe3', f'std::sqrt((fPtHe3 * std::cosh(fEtaHe3))*(fPtHe3 * std::cosh(fEtaHe3)) + {ParticleMasses["He"]}*{ParticleMasses["He"]})')
+      .Define(f'fEHad', f'std::sqrt((fPtHad * std::cosh(fEtaHad))*(fPtHad * std::cosh(fEtaHad)) + {ParticleMasses["Pr"]}*{ParticleMasses["Pr"]})')
+      .Define('fClusterSizeCosLamHe3', 'ComputeAverageClusterSize(fItsClusterSizeHe3) / cosh(fEtaHe3)')
+      .Define('fClusterSizeCosLamHad', 'ComputeAverageClusterSize(fItsClusterSizeHad) / cosh(fEtaHad)')
+      .Define('fExpectedClusterSizeHe3', 'ComputeExpectedClusterSizeCosLambdaHe(fPtHe3 * std::cosh(fEtaHe3))')
+      .Define('fExpectedClusterSizeHad', 'ComputeExpectedClusterSizeCosLambdaPr(fPtHad * std::cosh(fEtaHad))')
+      .Define('fNSigmaITSHe3', 'ComputeNsigmaITSHe(fPtHe3 * std::cosh(fEtaHe3), fClusterSizeCosLamHe3)')
+      .Define('fNSigmaITSHad', 'ComputeNsigmaITSPr(fPtHad * std::cosh(fEtaHad), fClusterSizeCosLamHad)')
+      .Redefine('fNSigmaTPCHe3', 'ComputeNsigmaTPCHe(std::abs(fInnerParamTPCHe3), fSignalTPCHe3, false, true)')
+      .Define('fNSigmaTPCPi', 'ComputeNsigmaTPCPi(std::abs(fInnerParamTPCHad), fSignalTPCHad)')
+      .Define('fNSigmaDCAxyHe3', 'ComputeNsigmaDCAxyHe(fPtHe3, fDCAxyHe3)')
+      .Define('fNSigmaDCAzHe3', 'ComputeNsigmaDCAzHe(fPtHe3, fDCAzHe3)')
+      .Define('fNSigmaDCAxyHad', 'ComputeNsigmaDCAxyPr(fPtHad, fDCAxyHad)')
+      .Define('fNSigmaDCAzHad', 'ComputeNsigmaDCAzPr(fPtHad, fDCAzHad)')
+      .Filter(base_selection).Filter(selection)
+      .Define('fKstar', f'ComputeKstar(fPtHe3, fEtaHe3, fPhiHe3, {ParticleMasses["He"]}, fPtHad, fEtaHad, fPhiHad, {ParticleMasses["Pr"]})')
+    )
     return rdf
 
-def purity_and_primary_fraction_histograms(rdf:RDataFrame, outfile:TFile,
-                                           input_primary_fraction_file: str, input_purity_file: str):
+def load_same(prepare_years:bool=True):
+    
+    config_same = {
+        '2023': {'input_data':'/data/galucia/lithium/same/LHC23_PbPb_pass5_hadronpid_same.root',},
+        '2024': {'input_data':'/data/galucia/lithium/same/LHC24ar_pass3_hadronpid_same.root',},
+        '2025': {'input_data':'/data/galucia/lithium/same/LHC25_PbPb_pass1_hadronpid_same.root',},
+    }
+    
+    if prepare_years:    
+        for year in config_same.keys():
+            config_same[year].update({
+                'tree_names':   ['O2he3hadtable', 'O2he3hadmult'],
+                'mode':         'DF',
+                'parameterisation': PARAMETRISATIONS[year],
+            })
+            
+            load_parametrisation(config_same[year])
+            chain_data_same, __ = prepare_input_tchain(config_same[year])
+            rdf_same = prepare_rdataframe(chain_data_same, base_selection, 'true')
+            rdf_same.Snapshot(f'rdf_same', f'/data/galucia/lithium/tmp/rdf_same_{year}.root')
+        
+    return RDataFrame('rdf_same', [f'/data/galucia/lithium/tmp/rdf_same_{year}.root' for year in config_same.keys()])
+
+def purity_and_primary_fraction_histograms(rdf:RDataFrame, outfile:TFile, input_file:str):
    
     graphs = {
         'Matter': {},
         'Antimatter': {}
     }
 
-    graphs['Matter']['primary_he3'] = load_graph(input_primary_fraction_file, 'He/DCAxy/g_primary_fraction_matter')
-    graphs['Matter']['primary_p'] = load_graph(input_primary_fraction_file, 'Pr/DCAxy/g_primary_fraction_matter')
-    graphs['Matter']['secondary_p_from_weak_decay'] = load_graph(input_primary_fraction_file, 'Pr/DCAxy/g_weak_decay_fraction_matter')
-    graphs['Matter']['purity_he3'] = load_graph(input_purity_file, 'He3/TPC/g_purity_matter')
-    graphs['Matter']['purity_p_tpc'] = load_graph(input_purity_file, 'Had/TPC/g_purity_matter')
-    graphs['Matter']['purity_p_tof'] = load_graph(input_purity_file, 'Had/TOF/g_purity_matter')
-    graphs['Antimatter']['primary_he3'] = load_graph(input_primary_fraction_file, 'He/DCAxy/g_primary_fraction_antimatter')
-    graphs['Antimatter']['primary_p'] = load_graph(input_primary_fraction_file, 'Pr/DCAxy/g_primary_fraction_antimatter')
-    graphs['Antimatter']['secondary_p_from_weak_decay'] = load_graph(input_primary_fraction_file, 'Pr/DCAxy/g_weak_decay_fraction_antimatter')
-    graphs['Antimatter']['purity_he3'] = load_graph(input_purity_file, 'He3/TPC/g_purity_antimatter')
-    graphs['Antimatter']['purity_p_tpc'] = load_graph(input_purity_file, 'Had/TPC/g_purity_antimatter')
-    graphs['Antimatter']['purity_p_tof'] = load_graph(input_purity_file, 'Had/TOF/g_purity_antimatter')
+    graphs['Matter']['primary_he3'] = load_graph(input_file, 'primary_fraction/He/g_primary_fraction_matter_averaged')
+    graphs['Matter']['primary_p'] = load_graph(input_file, 'primary_fraction/Pr/g_primary_fraction_matter_averaged')
+    graphs['Matter']['secondary_p_from_weak_decay'] = load_graph(input_file, 'primary_fraction/Pr/g_weak_decay_fraction_matter_averaged')
+    graphs['Matter']['purity_he3'] = load_graph(input_file, 'purity/He3/TPC/g_purity_matter_averaged')
+    graphs['Matter']['purity_p_tpc'] = load_graph(input_file, 'purity/Had/TPC/g_purity_matter_averaged')
+    graphs['Matter']['purity_p_tof'] = load_graph(input_file, 'purity/Had/TOF/g_purity_matter_averaged')
+    graphs['Antimatter']['primary_he3'] = load_graph(input_file, 'primary_fraction/He/g_primary_fraction_antimatter_averaged')
+    graphs['Antimatter']['primary_p'] = load_graph(input_file, 'primary_fraction/Pr/g_primary_fraction_antimatter_averaged')
+    graphs['Antimatter']['secondary_p_from_weak_decay'] = load_graph(input_file, 'primary_fraction/Pr/g_weak_decay_fraction_antimatter_averaged')
+    graphs['Antimatter']['purity_he3'] = load_graph(input_file, 'purity/He3/TPC/g_purity_antimatter_averaged')
+    graphs['Antimatter']['purity_p_tpc'] = load_graph(input_file, 'purity/Had/TPC/g_purity_antimatter_averaged')
+    graphs['Antimatter']['purity_p_tof'] = load_graph(input_file, 'purity/Had/TOF/g_purity_antimatter_averaged')
     
     h_protons_from_sigma = load_hist('/home/galucia/Lithium4/calibration/output/dca/dca_mc_template_check.root',
                                      'Pr/hPtPr_IsFromSigmaPlus')
@@ -205,19 +290,19 @@ def purity_and_primary_fraction_histograms(rdf:RDataFrame, outfile:TFile,
     ROOT.graph_sigma_protons_to_weak_decay_protons = g_sigma_protons_to_weak_decay_protons
 
     
-    rdf = rdf.Define('fPurityHe3', f'fSignedPtHe3 > 0 ? readFromTGraph(graph_purity_he3_matter, fPtHe3) : \
-                     readFromTGraph(graph_purity_he3_antimatter, fPtHe3)') \
+    rdf = (rdf.Define('fPurityHe3', f'fSignedPtHe3 > 0 ? readFromTGraph(graph_purity_he3_matter, fPtHe3) : \
+                      readFromTGraph(graph_purity_he3_antimatter, fPtHe3)')
              .Define('fPurityHad', f'fSignedPtHad > 0 ? (std::abs(fPtHad) < 0.8 ? readFromTGraph(graph_purity_p_tpc_matter, std::abs(fPtHad)) : readFromTGraph(graph_purity_p_tof_matter, std::abs(fPtHad))) : \
-                     (std::abs(fPtHad) < 0.8 ? readFromTGraph(graph_purity_p_tpc_antimatter, std::abs(fPtHad)) :readFromTGraph(graph_purity_p_tof_antimatter, std::abs(fPtHad)))') \
+                     (std::abs(fPtHad) < 0.8 ? readFromTGraph(graph_purity_p_tpc_antimatter, std::abs(fPtHad)) :readFromTGraph(graph_purity_p_tof_antimatter, std::abs(fPtHad)))')
              .Define('fPrimaryHe3', f'fSignedPtHe3 > 0 ? readFromTGraph(graph_primary_he3_matter, fPtHe3) : \
-                     readFromTGraph(graph_primary_he3_antimatter, fPtHe3)') \
+                     readFromTGraph(graph_primary_he3_antimatter, fPtHe3)')
              .Define('fPrimaryHad', f'fSignedPtHad > 0 ? readFromTGraph(graph_primary_p_matter, fPtHad) : \
-                     readFromTGraph(graph_primary_p_antimatter, fPtHad)') \
-             .Define('fSigmaProtonsToWeakDecayProtons', 'readFromTGraph(graph_sigma_protons_to_weak_decay_protons, fPtHad)') \
+                     readFromTGraph(graph_primary_p_antimatter, fPtHad)')
+             .Define('fSigmaProtonsToWeakDecayProtons', 'readFromTGraph(graph_sigma_protons_to_weak_decay_protons, fPtHad)')
              .Define('fSecondaryHadFromWeakDecay', f'fSignedPtHad > 0 ? readFromTGraph(graph_secondary_p_from_weak_decay_matter, fPtHad) : \
-                     readFromTGraph(graph_secondary_p_from_weak_decay_antimatter, fPtHad)') \
+                     readFromTGraph(graph_secondary_p_from_weak_decay_antimatter, fPtHad)')
              .Define('fSecondaryHadFromSigma', f'fSigmaProtonsToWeakDecayProtons * fSecondaryHadFromWeakDecay')
-             
+        )     
     
     all_hists = {}
     for sign in ['Both', 'Matter', 'Antimatter']:
@@ -284,31 +369,19 @@ def purity_and_primary_fraction_histograms(rdf:RDataFrame, outfile:TFile,
 
 
 if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(description='Compute lambda parameters for He3-p correlation analysis.')
+    parser.add_argument('--input', type=str, default='calibration/output/average_purity_and_primary_fraction.root', help='Path to the input configuration YAML file.')
+    parser.add_argument('--output', type=str, default='calibration/output/lambda_parameters.root', help='Path to the output ROOT file.')
+    args = parser.parse_args()
 
     proton_from_Sigma_to_proton_from_Lambda_ratio = 0.25
+    rdf = load_same(prepare_years=False)
+    rdf = rdf.Filter(base_selection)
 
-    config_file = '/home/galucia/Lithium4/preparation/config/config_prepare_PbPb.yml'
-    config = yaml.safe_load(open(config_file, 'r'))
-    
-    
-    #input_primary_fraction_file = '/home/galucia/Lithium4/calibration/output/dca/primary_fraction_results_new.root'
-    input_primary_fraction_file = '/home/galucia/Lithium4/calibration/output/dca/primary_fraction_results_new_smaller_tolerance_pr.root'
-    input_purity_file = '/home/galucia/DetectorCalibration/output/purity/LHC24ar_pass3_purity.root'
+    output_file = ROOT.TFile(args.output, "RECREATE")
 
-    load_parametrisation(config)  # Load parametrisation into Common.h
-    
-    selections = config.get('selections', [])
-    config = config['same_event']  # Use the specific mode section from the config
-
-    base_selection, selection = prepare_selections(selections)
-    chain_data, additional_chain_data = prepare_input_tchain(config)
-    rdf = prepare_rdataframe(chain_data, base_selection, selection)
-
-    #output_file_path = 'output/lambda_parameters.root'
-    output_file_path = 'output/LHC24ar_pass3_lambda_parameters.root'
-    output_file = ROOT.TFile(output_file_path, "RECREATE")
-
-    purity_and_primary_fraction_histograms(rdf, output_file, input_primary_fraction_file, input_purity_file)    
+    purity_and_primary_fraction_histograms(rdf, output_file, args.input)
 
     output_file.Close()
 
