c5ce1efe5706c86ca2e1b938170e0862c3e2fef2
GiorgioAlbertoLucia <giogioalberto@gmail.com>
2026-07-24 15:56:42 +0200

    plots drawing

--- preparation/efficiency.py
+++ preparation/efficiency.py
@@ -75,7 +75,7 @@ def prepare_input_tchain(config:dict):
 def prepare_rdataframe(chain_data: TChain, base_selection: str, selection: str):
    
     rdf = RDataFrame(chain_data)
-    print(tc.GREEN+'\nDataset columns'+tc.RESET)
+    print(tc.GREEN+'nDataset columns'+tc.RESET)
     print(tc.UNDERLINE+tc.CYAN+f'{rdf.GetColumnNames()}'+tc.RESET)
     
     # TPC
@@ -93,48 +93,55 @@ def prepare_rdataframe(chain_data: TChain, base_selection: str, selection: str):
       # Recalibration
       # Correct for PID in tracking
     
-    rdf_gen = rdf.Define('fSignedPtHe3', 'fPtHe3') \
-        .Define('fSignedPtHad', 'fPtHad') \
-        .Define('fSignedPtHe3MC', 'fPtMCHe3') \
-        .Define('fSignedPtHadMC', 'fPtMCHad') \
-        .Filter(base_selection) \
-        .Define('fSignHe3', 'fPtMCHe3/std::abs(fPtMCHe3)') \
-        .Define('fPtLiMC', 'std::abs(fSignedPtMC)') \
+    rdf_gen = (rdf.Define('fSignedPtHe3', 'fPtHe3') 
+        .Define('fSignedPtHad', 'fPtHad') 
+        .Define('fSignedPtHe3MC', 'fPtMCHe3') 
+        .Define('fSignedPtHadMC', 'fPtMCHad') 
+        .Filter(base_selection) 
+        .Define('fSignHe3', 'fPtMCHe3/std::abs(fPtMCHe3)') 
+        .Define('fPtLiMC', 'std::abs(fSignedPtMC)')
+    )
       
       
-    rdf_rec = rdf.Define('fSignedPtHad', 'fPtHad') \
-      .Define('fSignHe3', 'fPtHe3/std::abs(fPtHe3)') \
-      .Redefine('fPtHe3', 'std::abs(fPtHe3)') \
-      .Redefine('fPtHe3', '(fPIDtrkHe3 == 7) || (fPIDtrkHe3 == 8) || (fPtHe3 > 2.5) ? fPtHe3 : CorrectPidTrkHe(fPtHe3)') \
-      .Redefine('fPtHad', 'std::abs(fPtHad)') \
-      .Define('fSignedPtHe3', 'fPtHe3 * fSignHe3') \
-      .Define(f'fEHe3', f'std::sqrt((fPtHe3 * std::cosh(fEtaHe3))*(fPtHe3 * std::cosh(fEtaHe3)) + {ParticleMasses["He"]}*{ParticleMasses["He"]})') \
-      .Define(f'fEHad', f'std::sqrt((fPtHad * std::cosh(fEtaHad))*(fPtHad * std::cosh(fEtaHad)) + {ParticleMasses["Pr"]}*{ParticleMasses["Pr"]})') \
-      .Define('fDeltaEta', 'fEtaHe3 - fEtaHad') \
-      .Define('fDeltaPhi', 'fPhiHe3 - fPhiHad') \
-      .Redefine('fInnerParamTPCHe3', 'fInnerParamTPCHe3 * 2') \
-      .Redefine('fInnerParamTPCHe3', '(fPIDtrkHe3 == 7) || (fPIDtrkHe3 == 8) || (fPtHe3 > 2.5) ? fInnerParamTPCHe3 : CorrectPidTrkHe(fInnerParamTPCHe3, false)') \
-      .Redefine('fNSigmaTPCHe3', 'ComputeNsigmaTPCHe(std::abs(fInnerParamTPCHe3), fSignalTPCHe3, true, true)') \
-      .Define('fClusterSizeCosLamHe3', 'ComputeAverageClusterSize(fItsClusterSizeHe3) / cosh(fEtaHe3)') \
-      .Define('fClusterSizeCosLamHad', 'ComputeAverageClusterSize(fItsClusterSizeHad) / cosh(fEtaHad)') \
-      .Define('fExpectedClusterSizeHe3', 'ComputeExpectedClusterSizeCosLambdaHe(fPtHe3 * std::cosh(fEtaHe3))') \
-      .Define('fExpectedClusterSizeHad', 'ComputeExpectedClusterSizeCosLambdaPr(fPtHad * std::cosh(fEtaHad))') \
-      .Define('fNSigmaITSHe3', 'ComputeNsigmaITSHe(fPtHe3 * std::cosh(fEtaHe3), fClusterSizeCosLamHe3, true)') \
-      .Define('fNSigmaITSHad', 'ComputeNsigmaITSPr(fPtHad * std::cosh(fEtaHad), fClusterSizeCosLamHad, true)') \
-      .Define('fNSigmaDCAxyHe3', 'ComputeNsigmaDCAxyHe(fPtHe3, fDCAxyHe3, true)') \
-      .Define('fNSigmaDCAzHe3', 'ComputeNsigmaDCAzHe(fPtHe3, fDCAzHe3, true)') \
-      .Define('fNSigmaDCAxyHad', 'ComputeNsigmaDCAxyPr(fPtHad, fDCAxyHad, true)') \
-      .Define('fNSigmaDCAzHad', 'ComputeNsigmaDCAzPr(fPtHad, fDCAzHad, true)') \
-      .Filter(base_selection).Filter(selection) \
-      .Define('fPxLi', 'fPtHe3 * std::cos(fPhiHe3) + fPtHad * std::cos(fPhiHad)') \
-      .Define('fPyLi', 'fPtHe3 * std::sin(fPhiHe3) + fPtHad * std::sin(fPhiHad)') \
+    rdf_rec = (rdf.Define('fSignedPtHad', 'fPtHad') 
+      .Define('fSignHe3', 'fPtHe3/std::abs(fPtHe3)') 
+      .Redefine('fPtHe3', 'std::abs(fPtHe3)') 
+      .Redefine('fPtHe3', '(fPIDtrkHe3 == 7) || (fPIDtrkHe3 == 8) || (fPtHe3 > 2.5) ? fPtHe3 : CorrectPidTrkHe(fPtHe3)') 
+      .Redefine('fPtHad', 'std::abs(fPtHad)') 
+      .Define('fSignedPtHe3', 'fPtHe3 * fSignHe3') 
+      .Define(f'fEHe3', f'std::sqrt((fPtHe3 * std::cosh(fEtaHe3))*(fPtHe3 * std::cosh(fEtaHe3)) + {ParticleMasses["He"]}*{ParticleMasses["He"]})') 
+      .Define(f'fEHad', f'std::sqrt((fPtHad * std::cosh(fEtaHad))*(fPtHad * std::cosh(fEtaHad)) + {ParticleMasses["Pr"]}*{ParticleMasses["Pr"]})') 
+      .Define('fDeltaEta', 'fEtaHe3 - fEtaHad') 
+      .Define('fDeltaPhi', 'fPhiHe3 - fPhiHad') 
+      #.Redefine('fInnerParamTPCHe3', 'fInnerParamTPCHe3 * 2') 
+      #.Redefine('fInnerParamTPCHe3', '(fPIDtrkHe3 == 7) || (fPIDtrkHe3 == 8) || (fPtHe3 > 2.5) ? fInnerParamTPCHe3 : CorrectPidTrkHe(fInnerParamTPCHe3, false)') 
+      .Redefine('fNSigmaTPCHe3', 'ComputeNsigmaTPCHe(std::abs(fInnerParamTPCHe3), fSignalTPCHe3, true, true)') 
+      .Define('fClusterSizeCosLamHe3', 'ComputeAverageClusterSize(fItsClusterSizeHe3) / cosh(fEtaHe3)') 
+      .Define('fClusterSizeCosLamHad', 'ComputeAverageClusterSize(fItsClusterSizeHad) / cosh(fEtaHad)') 
+      .Define('fExpectedClusterSizeHe3', 'ComputeExpectedClusterSizeCosLambdaHe(fPtHe3 * std::cosh(fEtaHe3))') 
+      .Define('fExpectedClusterSizeHad', 'ComputeExpectedClusterSizeCosLambdaPr(fPtHad * std::cosh(fEtaHad))') 
+      .Define('fNSigmaITSHe3', 'ComputeNsigmaITSHe(fPtHe3 * std::cosh(fEtaHe3), fClusterSizeCosLamHe3, true)') 
+      .Define('fNSigmaITSHad', 'ComputeNsigmaITSPr(fPtHad * std::cosh(fEtaHad), fClusterSizeCosLamHad, true)') 
+      .Define('fNSigmaDCAxyHe3', 'ComputeNsigmaDCAxyHe(fPtHe3, fDCAxyHe3, true)') 
+      .Define('fNSigmaDCAzHe3', 'ComputeNsigmaDCAzHe(fPtHe3, fDCAzHe3, true)') 
+      .Define('fNSigmaDCAxyHad', 'ComputeNsigmaDCAxyPr(fPtHad, fDCAxyHad, true)') 
+      .Define('fNSigmaDCAzHad', 'ComputeNsigmaDCAzPr(fPtHad, fDCAzHad, true)') 
+      .Filter(base_selection).Filter(selection) 
+      .Define('fPxLi', 'fPtHe3 * std::cos(fPhiHe3) + fPtHad * std::cos(fPhiHad)') 
+      .Define('fPyLi', 'fPtHe3 * std::sin(fPhiHe3) + fPtHad * std::sin(fPhiHad)') 
+      .Define('fPzLi', 'fPtHe3 * std::sinh(fEtaHe3) + fPtHad * std::sinh(fEtaHad)')
       .Define('fPtLi', 'sqrt(fPxLi*fPxLi + fPyLi*fPyLi)')
+      .Define('fSignedPtLi', 'fPtLi * fSignHe3') 
+      .Define('fELi', 'fEHe3 + fEHad')
+      .Define('fPLi', 'sqrt(fPxLi*fPxLi + fPyLi*fPyLi + fPzLi*fPzLi)')
+      .Define('fMassInvLi', 'std::sqrt(fELi*fELi - fPLi*fPLi)')
+    )
       
     return rdf_rec, rdf_gen
 
 def visualise(rdf, output_file: TFile, mode:str='rec'):
    
-    print(f'\n{tc.GREEN}Creating histograms{tc.RESET}')
+    print(f'n{tc.GREEN}Creating histograms{tc.RESET}')
     histogram_registry = HistogramRegistry()
 
     if mode == 'rec':
@@ -146,7 +153,7 @@ def visualise(rdf, output_file: TFile, mode:str='rec'):
             if 'Pt' in entry.xvar:    entry.xvar += 'MC'
             histogram_registry.register(entry)
     
-    print(f'\tQA Histograms created!')
+    print(f'tQA Histograms created!')
         
     histogram_registry.prepare_directories(output_file)
     histogram_registry.draw_histogram(rdf)
@@ -178,13 +185,25 @@ def efficiency_histograms(rdf_rec, rdf_gen, output_file: TFile):
     hPtLiAntimatter = rdf_rec.Filter('fSignHe3 < 0').Histo1D(("hPtLiAntimatter", ";#it{p}_{T} (^{4}Li) (GeV/#it{c});", pt_bins, pt_min, pt_max), "fPtLi").GetValue()
     hPtLiMCMatter = rdf_gen.Filter('fSignHe3 > 0').Histo1D(("hPtLiMCMatter", ";#it{p}_{T} (^{4}#bar{Li}) (GeV/#it{c});", pt_bins, pt_min, pt_max), "fPtLiMC").GetValue()
     hPtLiMCAntimatter = rdf_gen.Filter('fSignHe3 < 0').Histo1D(("hPtLiMCAntimatter", ";#it{p}_{T} (^{4}#bar{Li}) (GeV/#it{c});", pt_bins, pt_min, pt_max), "fPtLiMC").GetValue()
+    
+    hPtLi = rdf_rec.Histo1D(("hPtLi", ";#it{p}_{T} (^{4}#bar{Li}) (GeV/#it{c});", 2*pt_bins, -pt_max, pt_max), "fSignedPtLi").GetValue()
+    hPtLi010 = rdf_rec.Filter('fCentralityFT0C < 10').Histo1D(("hPtLi010", "0-10% FT0C;#it{p}_{T} (^{4}#bar{Li}) (GeV/#it{c});", 2*pt_bins, -pt_max, pt_max), "fSignedPtLi").GetValue()
+    hPtLi1050 = rdf_rec.Filter('10 < fCentralityFT0C && fCentralityFT0C < 50').Histo1D(("hPtLi1050", "10-50% FT0C;#it{p}_{T} (^{4}#bar{Li}) (GeV/#it{c});", 2*pt_bins, -pt_max, pt_max), "fSignedPtLi").GetValue()
+    hPtLiMC = rdf_gen.Histo1D(("hPtLiMC", ";#it{p}_{T} (^{4}#bar{Li}) (GeV/#it{c});", 2*pt_bins, -pt_max, pt_max), "fSignedPtMC").GetValue()
+    hPtLiMC010 = rdf_gen.Filter('fCentralityFT0C < 10').Histo1D(("hPtLiMC010", "0-10% FT0C;#it{p}_{T} (^{4}#bar{Li}) (GeV/#it{c});", 2*pt_bins, -pt_max, pt_max), "fSignedPtMC").GetValue()
+    hPtLiMC1050 = rdf_gen.Filter('10 < fCentralityFT0C && fCentralityFT0C < 50').Histo1D(("hPtLiMC1050", "10-50% FT0C;#it{p}_{T} (^{4}#bar{Li}) (GeV/#it{c});", 2*pt_bins, -pt_max, pt_max), "fSignedPtMC").GetValue()
 
     hEfficiencyMatter = produce_efficiency_histogram(hPtLiMatter, hPtLiMCMatter, "hEfficiencyMatter")
     hEfficiencyAntimatter = produce_efficiency_histogram(hPtLiAntimatter, hPtLiMCAntimatter, "hEfficiencyAntimatter")
+    hEfficiency = produce_efficiency_histogram(hPtLi, hPtLiMC, "hEfficiency")
+    hEfficiency010 = produce_efficiency_histogram(hPtLi010, hPtLiMC010, "hEfficiency010")
+    hEfficiency1050 = produce_efficiency_histogram(hPtLi1050, hPtLiMC1050, "hEfficiency1050")
+    
 
-    output_file.cd()
-    for hist in [hPtLiMatter, hPtLiAntimatter, hPtLiMCMatter, hPtLiMCAntimatter,
-                 hEfficiencyMatter, hEfficiencyAntimatter]:
+    output_file.mkdir('Efficiency')
+    output_file.cd('Efficiency')
+    for hist in [hPtLiMatter, hPtLiAntimatter, hPtLiMCMatter, hPtLiMCAntimatter, hPtLi, hPtLi010, hPtLi1050, hPtLiMC, hPtLiMC010, hPtLiMC1050,
+                 hEfficiencyMatter, hEfficiencyAntimatter, hEfficiency, hEfficiency010, hEfficiency1050]:
         hist.Write()
 
 
